665cd0660842c6c09ba123d78f6973c287f3cf07
arthianandhan2829 <arthianandhan2829@gmail.com>
2026-03-10 06:47:54 -0400

    Add files via upload

---
+++ Cinemaprofit.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Cinemaprofit {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter ticketrevenue,snackrevenue,maintencecost,electricitybill:");
+        int ticketrevenue = sc.nextInt();
+        int snackrevenue = sc.nextInt();
+        int maintencecost = sc.nextInt();
+        int electricitybill = sc.nextInt();
+        int netprofit = ticketrevenue+snackrevenue-maintencecost-electricitybill;
+        System.out.println("Net profit =" + netprofit);
+    
+    
+    }
+}
