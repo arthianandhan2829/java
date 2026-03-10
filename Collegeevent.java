0eb1bf30e0e0a7686c1146738500a9b0b15e6469
arthianandhan2829 <arthianandhan2829@gmail.com>
2026-03-10 06:47:24 -0400

    Add files via upload

---
+++ Collegeevent.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Collegeevent {
+    public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter registration,sponsorship,stall,stage,celebrity,");
+    int registration = sc.nextInt();
+    int sponsorship = sc.nextInt();
+    int stall = sc.nextInt();
+    int stage = sc.nextInt();
+    int celebrity = sc.nextInt();
+    int remainingfund = registration+sponsorship+stall-stage-celebrity;
+      System.out.println("Remainingfund = "+remainingfund);
+    }
+}
