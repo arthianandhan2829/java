a85f42454140d4ec614bde7be284bbcf61c75159
arthianandhan2829 <arthianandhan2829@gmail.com>
2026-03-10 06:52:22 -0400

    Add files via upload

---
+++ Softwarecompany.java
@@ -0,0 +1,15 @@
+import  java.util.Scanner;
+public class Softwarecompany {
+public static void main(String[]args){
+   Scanner sc = new Scanner(System.in);
+   System.out.println("Enter hours,rate,design,testing,advance:");
+   int hours = sc.nextInt();
+   int rate = sc.nextInt();
+   int design = sc.nextInt();
+   int testing = sc .nextInt();
+   int advance = sc.nextInt();
+    int projectcost = (hours*rate)+design - testing - advance;
+    System.out.println("Final Praoject cost = "+ projectcost);
+
+}
+}
