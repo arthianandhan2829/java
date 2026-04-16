b6864e3d2a6914cf07068bcb69724645663e1370
arthianandhan2829 <arthianandhan2829@gmail.com>
2026-04-16 02:42:58 -0400

    Add files via upload

---
+++ Lambda.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N= sc.nextInt();
+        int arr[] = new int[N];
+        for(int i=0;i<N;i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0;i<N;i++){
+            if(arr[i] == 1){
+                System.out.print(arr[i]+" ");
+            }
+        }
+        for(int i=0;i<N;i++){
+            if(arr[i] == 0){
+                System.out.print(arr[i]+" ");
+            }
+        }
+        } 
+}
